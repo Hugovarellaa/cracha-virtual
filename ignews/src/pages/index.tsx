009838ae0f061eca9aeeaf1250eb7137011c843e
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-11-17 17:20:36 -0300

    .
    .

---
+++ ignews/src/pages/index.tsx
@@ -0,0 +1,57 @@
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
+import styles from "./home.module.scss";
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Home | Ig.news</title>
+      </Head>
+
+      <main className={styles.container}>
+        <section className={styles.hero}>
+          <span>👏 Hey Welcome</span>
+          <h1>
+            News about <br />
+            the <span>React</span> world
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for {product.amount} month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+        <img src="./images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1JwtdnLKV0dvLEEhv3Z8ENhq");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    redirect: 60 * 60 * 24, //24hours
+  };
+};
