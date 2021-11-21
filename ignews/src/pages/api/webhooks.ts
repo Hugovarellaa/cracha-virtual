6b7891e0e7d869323bfcd1cba97ef1166f95add6
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-11-20 23:43:55 -0300

    .
    .

--- ignews/src/pages/api/webhooks.ts
+++ ignews/src/pages/api/webhooks.ts
@@ -1,49 +1,45 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { Readable } from "stream";
+import { Readable } from "stream"
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
 
 async function buffer(readable: Readable) {
-  const chunks = [];
+  const chunks = []
 
   for await (const chunk of readable) {
-    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+    chunks.push(
+      typeof chunk === "string" ? Buffer.from(chunk) : chunk
+    );
+    return Buffer.concat(chunks)
   }
-  return Buffer.concat(chunks);
 }
 
 export const config = {
   api: {
-    bodyParser: false,
-  },
-};
+    bodyParser: false
+  }
+}
 
-const relevantEvents = new Set(["checkout.session.completed"]);
+const relevantEvents = new Set([
+  "checkout.session.completed"
+]);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
-    const buf = await buffer(req);
-    const secret = req.headers["stripe-signature"];
-    let event: Stripe.Event;
+    const buf = await buffer(req)
+    const secret = req.headers["stripe-signature"]
+
+    let event: Stripe.Event
     try {
-      event = stripe.webhooks.constructEvent(
-        buf,
-        secret,
-        process.env.STRIPE_WEBHOOK_SECRET
-      );
-    } catch (err) {
-      return res.status(400).send(`Webhook error: ${err.message}`);
+      event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET)
+    } catch (error) {
+      return res.status(400).send(`Webhook failed ${error.message}`)
     }
+    
+    const { type } = event
 
-    const { type } = event;
 
-    if (relevantEvents.has(type)) {
-      console.log("evento recebid", event);
-    } else {
-      console.log("nao recebeu");
-    }
-
-    res.json({ received: true });
+    return res.json({ received: true })
   } else {
     res.setHeader("Allow", "POST");
     res.status(405).end("Method not allowed in webhooks");
