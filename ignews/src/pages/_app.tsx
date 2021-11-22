55b9448bfe8d1fa6d956c7f3956b2db95b5b95a6
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-11-22 15:23:41 -0300

    .
    .

--- ignews/src/pages/_app.tsx
+++ ignews/src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import { AppProps } from "next/app";
-import "../styles/global.scss"
+import { Header } from "../components/Header";
+import "../styles/global.scss";
 
-function MyApp({ Component, pageProps }:AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- ignews/src/pages/_document.tsx
+++ ignews/src/pages/_document.tsx
@@ -11,6 +11,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Poppins:wght@400;500;600&display=swap"
             rel="stylesheet"
           />
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         </Head>
         <body>
           <Main />
