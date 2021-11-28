f20a48c84d240b1cbb9d2c65cad81ef1d50b1afe
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-11-28 12:18:38 -0300

    Delete ignews directory

--- ignews/src/services/fauna.ts
+++
@@ -1,6 +0,0 @@
-import FaunaDB from "faunadb"
-
-export const fauna = new FaunaDB.Client({
-  secret: process.env.FAUNADB_SECRET_KEY,
-
-})
--- ignews/src/services/stripe-js.ts
+++
@@ -1,6 +0,0 @@
-import { loadStripe } from "@stripe/stripe-js";
-
-export async function getStripeJS() {
-  const stripeJs = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY);
-  return stripeJs
-}
