fca59d390a7e85f81c07bb2fce748bea26f915e7
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-11-29 16:40:22 -0300

    Chapter IV
    Autenticação com JWT
    
     - Contexto de autenticação
     - Configurando cliente do axios

--- nextauth/contexts/AuthContext.tsx
+++ nextauth/contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode } from "react";
+import { api } from "../services/api";
 
 interface AuthProviderProps {
   children: ReactNode;
@@ -19,7 +20,15 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const isAuthenticated = false;
 
   async function signIn({ email, password }: SignInCredentials) {
-    console.log({email, password});
+    try{
+      const response = await api.post('/sessions' , {
+        email,
+        password
+      })
+      console.log(response.data)
+    }catch(err){
+      console.log(err)
+    }
   }
 
   return (
