222924b1bbd6f153ded08d0508f4a6efc4e482f2
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-11-29 17:29:09 -0300

    Chapter IV - Autenticação com JWT
     - Contexto de autenticação
     - Configurando cliente do axios
     - Salvando dados do usuário
     - Salvando tokens nos cookies

--- nextauth/contexts/AuthContext.tsx
+++ nextauth/contexts/AuthContext.tsx
@@ -1,5 +1,13 @@
-import { createContext, ReactNode } from "react";
+import { parseNextUrl } from "next/dist/shared/lib/router/utils/parse-next-url";
+import Router from "next/router";
+import { createContext, ReactNode, useState } from "react";
 import { api } from "../services/api";
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
 
 interface AuthProviderProps {
   children: ReactNode;
@@ -12,27 +20,36 @@ type SignInCredentials = {
 type AuthContextData = {
   signIn: (credentials: SignInCredentials) => Promise<void>;
   isAuthenticated: boolean;
+  user: User;
 };
 
 export const AuthContext = createContext({} as AuthContextData);
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  const isAuthenticated = false;
+  const [user, setUser] = useState<User>();
+  const isAuthenticated = !!user;
 
   async function signIn({ email, password }: SignInCredentials) {
-    try{
-      const response = await api.post('/sessions' , {
+    try {
+      const response = await api.post("/sessions", {
         email,
-        password
-      })
-      console.log(response.data)
-    }catch(err){
-      console.log(err)
+        password,
+      });
+      const { token, refreshToken, permissions, roles } = response.data;
+
+      setUser({
+        email,
+        permissions,
+        roles,
+      });
+      Router.push("/dashboard");
+    } catch (err) {
+      console.log(err);
     }
   }
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, signIn }}>
+    <AuthContext.Provider value={{ isAuthenticated, signIn, user }}>
       {children}
     </AuthContext.Provider>
   );
--- nextauth/pages/dashboard.tsx
+++ nextauth/pages/dashboard.tsx
@@ -1,7 +1,11 @@
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+
 export default function Dashboard() {
+  const { user } = useContext(AuthContext);
   return (
     <>
-      <h1>testando: </h1>
+      <h1>Dashboard: {user?.email}</h1>
     </>
   );
 }
